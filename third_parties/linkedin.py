a2e5f64902491329ae0ce4068fedd47c5a68b808
ymarcal <ymarcal333@gmail.com>
2025-08-24 15:25:46 -0300

    development

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -4,13 +4,42 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LINKEDIN_API_URL = "https://api.linkedin.com/v2"
-LINKEDIN_ACCESS_TOKEN = os.getenv("LINKEDIN_ACCESS_TOKEN")
+# Posso tentar achar uma API grátis para fazer o scraping
 
-def get_linkedin_profile():
-    headers = {
-        "Authorization": f"Bearer {LINKEDIN_ACCESS_TOKEN}",
-        "Content-Type": "application/json"
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from a LinkedIn profile,
+    Manually scrape the information from the LinkedIn profile page."""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/ymarcal/3bc6342912265a34f6def3b21a3c406a/raw/0237c2a3583ca045999faacb0b5aaa73c4b4f785/jonatan-lima-scrapin.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10
+        )
+    else:
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ['SCRAPIN_API_KEY'],
+            "linkedInUrl": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params=params,
+            timeout=10
+        )
+    data = response.json().get("person")
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None) and k not in ["certifications"]
     }
-    response = requests.get(f"{LINKEDIN_API_URL}/me", headers=headers)
-    return response.json()
+
+    return data
+
+if __name__ == '__main__':
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/jonatan-de-lima-santos-852j/", mock=True
+        )
+    )
